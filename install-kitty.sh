a8d9c32fbb40d8d0e641ff8fd43152a2adca97b7
Sokun Chorn <chornsokun@gmail.com>
2021-03-07 21:28:41 +1100

    install fira code

--- install-kitty.sh
+++ install-kitty.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo su -
-apt-get install kitty git -y
+apt-get install kitty git fonts-firacode -y
 exit
 
+mkdir -p ~/.config/kitty
 git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
 rsync -r .config/kitty ~/.config/kitty
